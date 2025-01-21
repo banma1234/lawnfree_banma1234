5abf3aff5671cce15d4c1dc913644624caf89299
chocoham <banma1234@gmail.com>
2025-01-22 02:57:49 +0900

    feat: shadCN components 추가

---
+++ components/ui/index.ts
@@ -0,0 +1,23 @@
+import { Button } from "./button";
+import { Card } from "./card";
+import { Input } from "./input";
+import { Toast } from "./toast";
+import { Toaster } from "./toaster";
+import { Textarea } from "./textarea";
+import { Table } from "./table";
+import { Select } from "./select";
+
+const Components = {
+  Button,
+  Card,
+  Input,
+  Table,
+  Toast,
+  Toaster,
+  Textarea,
+  Select,
+};
+
+export default Components;
+
+export { Button, Card, Input, Table, Toast, Toaster, Textarea, Select };
