953ba9018b69dd07bd0c4b58d1b4aeb7b469028b
chocoham <banma1234@gmail.com>
2025-01-23 02:14:00 +0900

    refactor: 폴더구조 수정, alert-dialog 추가

---
+++ ui/index.ts
@@ -0,0 +1,21 @@
+import { Button } from "./shadCn/button";
+import { Card } from "./shadCn/card";
+import { Input } from "./shadCn/input";
+import { Toast } from "./shadCn/toast";
+import { Toaster } from "./shadCn/toaster";
+import { Textarea } from "./shadCn/textarea";
+import { Select } from "./shadCn/select";
+
+const Components = {
+  Button,
+  Card,
+  Input,
+  Toast,
+  Toaster,
+  Textarea,
+  Select,
+};
+
+export default Components;
+
+export { Button, Card, Input, Toast, Toaster, Textarea, Select };
