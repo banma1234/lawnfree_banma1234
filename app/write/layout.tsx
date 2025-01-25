c1cf5da8d0b54d993b8c7398bc92ad545252cc68
chocoham <banma1234@gmail.com>
2025-01-26 01:55:35 +0900

    feat: 동적 meta-data 작성 및 디자인 최적화

--- app/write/layout.tsx
+++ app/write/layout.tsx
@@ -6,12 +6,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="md:w-3/5 sm:4/5 h-full mt-8 rounded-xl overflow-hidden text-stone-800">
+    <section className="md:w-4/5 max-w-[860px] sm:w-5/6 h-full mt-8 rounded-xl overflow-hidden text-stone-800">
       {children}
     </section>
   );
 }
 
+/**
+ * `동적`으로 meta 데이터 생성 및 반환. 게시글 `수정`의 경우 대상 포스트의 정보 적용
+ * @returns `meta 데이터`
+ */
 export async function generateMetadata() {
   const postId = (await headers()).get("x-path-postid");
   const title = (await headers()).get("x-path-title");
@@ -24,7 +28,7 @@ export async function generateMetadata() {
   }
 
   return {
-    title: `(수정 중) ${title}`,
+    title: `(수정 중) ${decodeURI(title)}`,
     description: "기존 글을 수정 중입니다.",
   };
 }
--- app/write/page.tsx
+++ app/write/page.tsx
@@ -2,15 +2,19 @@
 
 import React, { useState, useEffect, useCallback } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { parseDate } from "../utils/parseDate";
+import { handleSVG } from "../utils/handleSVG";
+import { toast } from "../utils/hooks/use-toast";
+import { Button, Input, Textarea } from "@/ui";
 import {
   usePostState,
   usePostStateInput,
   usePostStateEdit,
 } from "../utils/hooks/store/usePostStore";
-import { handleSVG } from "../utils/handleSVG";
-import { toast } from "../utils/hooks/use-toast";
-import { Button, Input, Textarea } from "@/ui";
 
+/**
+ * 게시글 `작성`/`수정` 페이지
+ */
 export default function Write() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
@@ -21,16 +25,24 @@ export default function Write() {
 
   const router = useRouter();
   const searchParams = useSearchParams().get("postid");
+  /** 가장 `마지막` 포스트의 `postId` + 1 */
   const postLength = usePostState().length + 1;
+  /** `true` : 신규작성 / `false` : 게시물 수정 */
   const isNewPost = searchParams ? false : true;
 
+  /**
+   * 쿼리값 참고하여 `수정`할 포스트의 내용 불러오는 함수
+   */
   const getPostData = useCallback(() => {
+    // 신규작성일 경우 return
     if (!searchParams) return;
 
     const targetPost = localStorage.getItem(`post-${searchParams}`);
 
     if (!targetPost) {
-      setError("그런거 없다");
+      setError(
+        `해당하는 포스트가 존재하지 않습니다 | postId : ${searchParams}`
+      );
       return;
     }
     const storedData = JSON.parse(targetPost);
@@ -40,26 +52,36 @@ export default function Write() {
   }, [searchParams]);
 
   useEffect(() => {
+    getPostData();
+  }, [getPostData]);
+
+  /**
+   * 에러 핸들링
+   */
+  useEffect(() => {
     if (!error.length) return;
 
     toast({ variant: "destructive", title: `${error}` });
     setError("");
   }, [error]);
 
-  useEffect(() => {
-    getPostData();
-  }, [getPostData]);
-
+  /**
+   * 작성을 끝마친 포스트를 `추가`/`수정`하는 함수.
+   * @param e 추가입력 방지
+   */
   const submitPost = (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    if (!title || !content) return setError("제목 / 내용을 입력해주세요");
+    if (!title) return setError("제목을 입력해주세요");
+    if (!content) return setError("내용을 입력해주세요");
 
+    // `postStore`에 저장될 게시물 정보
     const postInfo = {
       postId: isNewPost ? postLength : Number(searchParams),
       title,
-      uploadDate: new Date(),
+      uploadDate: parseDate(new Date()),
     };
 
+    // 플래그 `isNewPost`을 기준으로 작성/수정 분기
     if (!isNewPost) {
       editPost(postInfo, content);
     } else {
@@ -78,19 +100,20 @@ export default function Write() {
       <Input
         type="text"
         placeholder="제목을 입력하세요."
-        className="w-full h-auto text-3xl p-6 font-bold rounded-none whitespace-normal line-clamp-1 border-none focus-visible:ring-0 bg-stone-100"
+        className="w-full h-auto text-2xl sm:text-3xl p-6 font-bold rounded-none whitespace-normal line-clamp-1 border-none focus-visible:ring-0 bg-stone-100"
         value={title}
-        onChange={e => setTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <Textarea
         placeholder="내용을 입력하세요."
-        className="h-[480px] text-l border-dashed rounded-none border-t-purple-400 p-6 focus-visible:ring-0 resize-none bg-stone-100"
+        className="h-[480px] text-l border-dashed rounded-none border-stone-300 border-x-0 p-6 focus-visible:ring-0 resize-none bg-stone-100"
         value={content}
-        onChange={e => setContent(e.target.value)}
+        wrap="hard"
+        onChange={(e) => setContent(e.target.value)}
       />
       <div className="flex justify-end mt-2">
         <Button
-          className="w-[142px] h-[54px] text-xl rounded-xl bg-purple-600 hover:bg-purple-800"
+          className="w-[142px] h-[54px] text-xl rounded-xl font-bold bg-purple-600 hover:bg-purple-800"
           onClick={submitPost}
         >
           {handleSVG("CHECK", "30")} 작성 완료
