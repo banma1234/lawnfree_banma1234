f7c9e792f9adbd230392ba6604f1a4fc1199de64
chocoham <banma1234@gmail.com>
2025-01-26 01:54:11 +0900

    refactor: children 추가 및 디자인 최적화

--- app/components/AlertRemovePost.tsx
+++ app/components/AlertRemovePost.tsx
@@ -11,20 +11,38 @@ import {
 } from "@/ui/shadCn/alert-dialog";
 import { toast } from "@/app/utils/hooks/use-toast";
 import { usePostStateRemove } from "@/app/utils/hooks/store/usePostStore";
-import { handleSVG } from "../utils/handleSVG";
+import { useRouter, usePathname } from "next/navigation";
+import { ReactNode } from "react";
 
-export default function AlertRemovePost(props: { postId: number }) {
+/**
+ * 대상 포스트를 삭제하고 `Alert-Dialog`창을 띄우는 컴포넌트
+ * @param props 화면에 출력될 `ReactNode`와 삭제할 포스트의 `postId`
+ */
+export default function AlertRemovePost(props: {
+  postId: number;
+  children: ReactNode;
+}) {
+  const router = useRouter();
+  const pathName = usePathname();
   const removePostById = usePostStateRemove();
 
+  /**
+   * 대상 포스트를 삭제하고 현재 페이지의 위치에 따라 라우팅 진행
+   * @param postId 삭제할 포스트의 `postId`
+   */
   const removeTargetPost = (postId: number) => {
     removePostById(postId);
     toast({ title: "포스트가 삭제되었습니다" });
+
+    if (pathName !== "/") {
+      router.replace("/");
+    }
   };
 
   return (
     <AlertDialog>
-      <AlertDialogTrigger asChild className="ml-1.5 hover:text-stone-700">
-        {handleSVG("DELETE", "18")}
+      <AlertDialogTrigger asChild className="hover:text-stone-500">
+        {props.children}
       </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
--- app/components/PostTable.tsx
+++ app/components/PostTable.tsx
@@ -3,20 +3,28 @@
 import { Table, TableBody, TableRow, TableCell } from "@/ui/shadCn/table";
 import { usePostState } from "../utils/hooks/store/usePostStore";
 import { useSearchParams, useRouter } from "next/navigation";
+import { handleSVG } from "../utils/handleSVG";
+import { PostInfo } from "../types/postType";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import AlertRemovePost from "./AlertRemovePost";
-import { parseDate } from "../utils/parseDate";
-import { PostInfo } from "../types/postType";
+import Image from "next/image";
 
+/**
+ * 게시물 전체 `목록`을 반환하는 컴포넌트
+ */
 export default function PostTable() {
-  const [loadedPosts, setLoadedPosts] = useState<PostInfo[]>();
+  const [loadedPosts, setLoadedPosts] = useState<PostInfo[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const postState = usePostState();
   const router = useRouter();
+  /** 검색어 쿼리값 */
   const searchParams = useSearchParams().get("q");
 
+  /**
+   * 데이터 로딩
+   */
   useEffect(() => {
     if (postState) {
       setIsLoading(true);
@@ -24,7 +32,11 @@ export default function PostTable() {
     }
   }, [postState]);
 
+  /**
+   * 쿼리 스트링값에 변동 발생시 게시물 검색 수행
+   */
   useEffect(() => {
+    // 검색창이 비워졌을 때. 즉 `input`값이 없을 시 기존 데이터로 덮어씌운 후 메인화면으로 라우트
     if (!searchParams) {
       setLoadedPosts(postState);
       router.replace("/");
@@ -34,25 +46,33 @@ export default function PostTable() {
     searchEngine(searchParams);
   }, [searchParams]);
 
+  /**
+   * `input`값에 따라 검색을 수행하는 로직
+   * @param input 검색창 입력값
+   */
   const searchEngine = (input: string) => {
+    // 게시물 로딩 미완료시 return
     if (!loadedPosts) return;
 
-    let targetData = postState.filter(item => item.title.includes(input));
+    const targetData = postState.filter((item) => item.title.includes(input));
 
     if (!targetData.length) {
+      // 해당사항 없으면 빈 배열 반환
       setLoadedPosts([]);
     } else {
       setLoadedPosts(targetData);
     }
   };
 
-  if (!isLoading || !loadedPosts) return <Skeleton />;
+  // 로딩중일 경우 스켈레톤 ui 반환
+  if (!isLoading) return <Skeleton />;
+  // 로딩은 완료됬으나 포스트가 없는 경우 notfound 반환
   if (isLoading && !loadedPosts.length) return <PostNotFound />;
 
   return (
     <Table className="w-full">
       <TableBody>
-        {loadedPosts.map(post => (
+        {loadedPosts.map((post) => (
           <TableRow key={post.postId}>
             <TableCell className="w-[60px] text-center">
               {post.postId}
@@ -66,10 +86,12 @@ export default function PostTable() {
               </Link>
             </TableCell>
             <TableCell className="text-center w-[80px]">
-              {parseDate(post.uploadDate)}
+              {post.uploadDate}
             </TableCell>
             <TableCell className="w-[50px] cursor-pointer">
-              <AlertRemovePost postId={post.postId} />
+              <AlertRemovePost postId={post.postId}>
+                {handleSVG("DELETE", "18")}
+              </AlertRemovePost>
             </TableCell>
           </TableRow>
         ))}
@@ -78,11 +100,14 @@ export default function PostTable() {
   );
 }
 
+/**
+ * 메인화면 로딩중 나타나는 스켈레톤 ui
+ */
 const Skeleton = () => {
   return (
     <div className="w-full h-[500px] flex flex-col">
       <div className="w-full h-[37px] flex flex-col justify-between">
-        {[1, 2, 3, 4].map((item, i) => (
+        {[1, 2, 3, 4, 5].map((item, i) => (
           <div key={i} className="flex flex-row">
             <div className="w-[60px] p-2">
               <div className="bg-stone-100 rounded-md">&nbsp;</div>
@@ -103,10 +128,18 @@ const Skeleton = () => {
   );
 };
 
+/**
+ * 포스트가 없을 경우 출력되는 이미지
+ * @returns post_not_found.svg
+ */
 const PostNotFound = () => {
   return (
-    <div>
-      <h1>아무것도 없다 임마</h1>
-    </div>
+    <Image
+      className="mt-6"
+      src={"/post_not_found.svg"}
+      width={"700"}
+      height={"420"}
+      alt="엑박 이미지"
+    />
   );
 };
